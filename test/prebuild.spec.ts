b3c63facf5d04f515018067cf4c4e78788fcd0ea
brandonxiang <1542453460@qq.com>
2020-01-30 13:51:22 +0800

    build: add travis

--- test/prebuild.spec.ts
+++ test/prebuild.spec.ts
@@ -1,16 +1,31 @@
 import NextjsComponent from '../src/serverless';
 import fs from 'fs';
+import path from 'path';
+
+const existFile = (pathstr: string) => fs.existsSync(path.resolve(pathstr));
 
 test('prebuild', () => {
-  // console.log(NextjsComponent);
   const nextjsComponent = new NextjsComponent();
   nextjsComponent.prepareBuildManifests({
     name: 'webh5',
     nextConfigDir: 'test/fixtures/prebuild'
   });
 
-  const existIndex = fs.existsSync(
-    './fixtures/prebuild/.next/serverless/index.js'
+  console.log(
+    path.resolve('./test/fixtures/prebuild/.next/serverless/index.js')
   );
+
+  const existIndex = existFile(
+    './test/fixtures/prebuild/.next/serverless/index.js'
+  );
+  const existUtils = existFile(
+    './test/fixtures/prebuild/.next/serverless/utils.js'
+  );
+  const existManifest = existFile(
+    './test/fixtures/prebuild/.next/serverless/manifest.json'
+  );
+
   expect(existIndex).toBe(true);
+  expect(existUtils).toBe(true);
+  expect(existManifest).toBe(true);
 });
